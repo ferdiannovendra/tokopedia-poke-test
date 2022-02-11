1590f03712f3ce660c7c1655b520247eb2965721
ferdiannovendra <ferdiannovendra15@gmail.com>
2022-02-11 17:22:50 +0700

    mantap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {useState, useEffect, Fragment} from 'react';
+import { getAllPokemon, getPokemon } from './containers/PokemonsContainer';
+import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadingPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, [])
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord; 
+    }))
+    
+    setPokemonData(_pokemonData);
+  }
+
+  console.log(pokemonData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Fragment>
+    <Navbar />
+
+    <div>
+      { loading ? <h1>Loading...</h1> : (
+        <div className='grid-container'>
+          {pokemonData.map((pokemon, i) => {
+            return <Card key={i} pokemon={pokemon} />
+          })}
+        </div>
+      )}
     </div>
-  );
+    </Fragment>
+  )
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            <div className='item'>Pokemon API</div>
+            <div className='item'>My Pokemon</div>
+        </div>
+    );
+}
+
+export default Navbar;
